2509c1d1306626b3c8a62568c13e8e8934bfee3a
Francisco Macedo <franciscovcbm@gmail.com>
2023-12-20 10:20:58 +0000

    remove base url from env secret

--- scraper/settings.py
+++ scraper/settings.py
@@ -1,6 +1,8 @@
 from decouple import config
 
-BASE_URL = config("BASE_URL")
+
 X_IG_APP_ID = config("X_IG_APP_ID")
 SESSION_ID = config("SESSION_ID")
+
+BASE_URL = "https://raw.githubusercontent.com/franciscobmacedo/caffs/main"
 IMAGES_RELATIVE_PATH = "frontend/images"
